dbe1a7e9a841d6b8d918f0da1644e068c2658dc8
Labnan <khalidmasum@iut-dhaka.edu>
2020-09-15 10:19:03 +0600

    Refactor: removing redundant smallCells removed smallCells from BoardTest #1

--- solutions/33-labnan/TicTacToe/maingame/BoardTest.java
+++ solutions/33-labnan/TicTacToe/maingame/BoardTest.java
@@ -4,15 +4,8 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 
 public class BoardTest {
-    SmallCell[][] smallCells;
+
     Board board = new Board();
-
-
-    private void initializeSmallCells() {
-        smallCells = board.getSmallCells();
-    }
-
-
 
 
     private void simultaneouslyTrigger(int i, int j, PlayerMark playerMark) {
@@ -22,7 +15,6 @@ public class BoardTest {
 
     @Test
     public void testBoardChangeListener(){
-        initializeSmallCells();
         final int[] count = {0};
         board.addOnChangeListener(() -> count[0]++);
         board.triggerSquareAt(new Position(0,0),PlayerMark.AI);
@@ -31,14 +23,12 @@ public class BoardTest {
 
     @Test
     public void testAITrigger() {
-        initializeSmallCells();
         simultaneouslyTrigger(0, 1,PlayerMark.AI);
         Assertions.assertEquals(PlayerMark.AI, board.getMarkAtPosition(new Position(0,1)));
     }
 
     @Test
     public void testHumanTrigger() {
-        initializeSmallCells();
         simultaneouslyTrigger(0, 1,PlayerMark.HUMAN);
         Assertions.assertEquals(PlayerMark.HUMAN,board.getMarkAtPosition(new Position(0,1)));
         Assertions.assertNull(board.getMarkAtPosition(new Position(2, 2)));
